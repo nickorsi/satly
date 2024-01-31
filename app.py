94a84bb74368b1d29326d5b3565de459eca8deac
Matt Smida <mcs@m4t7.com>
2024-01-31 14:48:56 -0500

    added incantations and verified that upload to bucket works

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 import os
-# from flask import Flask
 from dotenv import load_dotenv
 import boto3
+from flask import Flask, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Photo
 
 load_dotenv()
 
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = AWS_SECRET_ACCESS_KEY
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+"DATABASE_URL", 'postgresql:///saltly')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+connect_db(app)
+
+app.debug = True  # False to turn off FDT
+debug = DebugToolbarExtension(app)
+
 
 # trying to test boto3. ... fn to get object here.
 
@@ -19,7 +34,7 @@ s3 = boto3.client(
 # Uploading a file
 s3.upload_file(
     './static/Konica_Minolta_DiMAGE_Z3.jpg',
-    'saltly-bucket',
+    'saltly',    # or saltly-bucket
     'Konica_Minolta_DiMAGE_Z3.jpg'
 )
 
