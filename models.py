7cb516a5ee29573c4c14ff873a0864ee4429392c
Matt Smida <mcs@m4t7.com>
2024-01-31 14:50:20 -0500

    defined fields for photos table

---
+++ models.py
@@ -0,0 +1,50 @@
+""" Models for saltly """
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Photo(db.Model):
+    """ Photos model """
+
+    __tablename__ = 'photos'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    title = db.Column(
+        db.String(300),
+        nullable=False
+    )
+
+    caption = db.Column(
+        db.String(700),
+        nullable=False
+    )
+
+    s3_photo_url = db.Column(
+        db.String(700),
+        nullable=False
+    )
+
+    # s3_thumbnail_url = db.Column(
+    #     db.String(700),
+    #     nullable=False
+    # )
+
+    # exif_data = db.Column(
+    #     db.String(1000)
+    # )
+
+
+
+def connect_db(app):
+    """
+    Connect this database to provided Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
