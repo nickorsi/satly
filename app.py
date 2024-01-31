df8106d364ab00c7e63af4622d73c254ea21d3a1
Matt Smida <mcs@m4t7.com>
2024-01-31 17:14:32 -0500

    added template names in render template calls in routes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-import boto3
+# import boto3
 from flask import Flask, redirect, render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -26,18 +26,18 @@ debug = DebugToolbarExtension(app)
 
 # trying to test boto3. ... fn to get object here.
 
-s3 = boto3.client(
-    "s3",
-    "us-west-1",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-)
+# s3 = boto3.client(
+#     "s3",
+#     "us-west-1",
+#     aws_access_key_id=AWS_ACCESS_KEY_ID,
+#     aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+# )
 # Uploading a file, WILL REPLACE/OVERWRITE IF SAME OBJECT KEY NAME
-s3.upload_file(
-    './static/Konica_Minolta_DiMAGE_Z3.jpg',
-    S3_BUCKET,    # or saltly-bucket
-    'Konica_Minolta_DiMAGE_Z3.jpg'
-)
+# s3.upload_file(
+#     './static/Konica_Minolta_DiMAGE_Z3.jpg',
+#     S3_BUCKET,    # or saltly-bucket
+#     'Konica_Minolta_DiMAGE_Z3.jpg'
+# )
 
 # Get URL in S3, which will be BASE_URL + the object key (filename)
 
@@ -46,7 +46,7 @@ s3.upload_file(
 def home():
     """Displays homepage"""
 
-    return render_template("HOMEPAGE TEMPLATE")  # TODO: Add template name
+    return render_template("home.html")
 
 
 @app.get("/photos")
@@ -55,8 +55,7 @@ def photos():
 
     photos = Photo.query.filter_by(active=True).all()
 
-    # TODO: Add template name
-    return render_template("PHOTOS PAGE", photos=photos)
+    return render_template("photos.html", photos=photos)
 
 
 @app.get("/photos/<int:photo_id>")
