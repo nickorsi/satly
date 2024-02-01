7d77051b2264b9138a91a7577f0141df353a22ad
Nicholas <nick.orsi1992@gmail.com>
2024-02-01 14:47:46 -0800

    Added code to grab photo from external source S3 and manipulate the photo to be black and white. Opens as a temp file.

--- pillow.py
+++ pillow.py
@@ -1,7 +1,14 @@
 from PIL import Image
+import requests
 
-file = './static/matt.jpg'
-img = Image.open(file)
+response = requests.get("https://saltly-bucket.s3.us-west-1.amazonaws.com/Nikon_COOLPIX_P1.jpg", stream=True)
+
+with open("./static/Nikon_COOLPIX_P1.jpg", "wb") as outfile:
+    outfile.write(response.content)
+
+    breakpoint()
+    # file = 'https://saltly-bucket.s3.us-west-1.amazonaws.com/Nikon_COOLPIX_P1.jpg'
+img = Image.open("./static/Nikon_COOLPIX_P1.jpg")
 
 img_bw = img.convert("L")
 img.show()
