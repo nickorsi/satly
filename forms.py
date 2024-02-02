3e98d51c4cefd3534c31872257d997fabc5dfe14
Nicholas <nick.orsi1992@gmail.com>
2024-02-02 11:34:29 -0800

    minor coment change in forms and reorganize styling sheet.

--- forms.py
+++ forms.py
@@ -4,11 +4,6 @@ from wtforms import StringField, FileField, BooleanField
 
 from wtforms.validators import InputRequired, Optional, Length
 
-# def file_type_check(form, field):
-#     """Checks input file type against accepted file types"""
-
-#     if field.data
-# TODO: Validate file type.
 
 
 class AddPhotoForm(FlaskForm):
